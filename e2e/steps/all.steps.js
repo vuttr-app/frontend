40140a3a4f1623618fdcbaa01c8bf22e68f0e71d
mestihudson <mestihudson@gmail.com>
2019-08-23 23:42:41 -0300

    Changes many stuffs
      - Add license field on 'e2e/package.json';
      - Move 'e2e/step-definitions' to 'e2e/steps';
      - Rename 'e2e/steps/listar-todas-as-ferramentas.steps.js' to
        'e2e/steps/all.steps.js';
      - Rename 'e2e/features/listar-todas-as-ferramentas.feature' to
        'e2e/features/list-all-tools.feature';
      - Rename 'e2e/features/remover-ferramenta.feature' to
        'e2e/steps/remove-tool.feature';
      - Rename 'e2e/features/buscar-ferramentas.feature' to
        'e2e/features/search-tool.feature';
      - Rename 'e2e/features/add-new-tool.feature' to
        'e2e/features/add-new-tool.feature';
      - Changes all feature files to scenario outline approach;
      - Translate all feature files and step descriptions from PT to EN.

--- e2e/steps/all.steps.js
+++ e2e/steps/all.steps.js
@@ -6,27 +6,27 @@ import Frontend from './frontend'
 let backend = new Backend('/ws/api/.db.json')
 let frontend = new Frontend(process.env.APP_URL)
 
-Given('que existe uma ferramenta', async () => {
+Given('that there is one tool', async () => {
   backend.registrar({ tools: [ { id: 1, title: 'Ferramenta 1' } ] })
 })
 
-When('eu solicito a lista de todas as ferramentas', async () => {
+When('i request a list of all tools', async () => {
   await frontend.entrar()
 })
 
-Then('eu verifico que a lista apresentada possui uma ferramenta', async () => {
+Then('i verify that one tool is presented', async () => {
   expect((await frontend.ferramentas()).length).to.be.equals(1)
 })
 
-Given('que não existem ferramentas', async () => {
+Given('that there is no one tool', async () => {
   backend.registrar({ tools: [] })
 })
 
-Then('eu verifico que nenhuma ferramenta é apresentada', async () => {
+Then('i verify that none tool is presented', async () => {
   expect((await frontend.ferramentas()).length).to.be.equals(0)
 })
 
-Given('que existe mais que uma ferramenta', async () => {
+Given('that there is more than one tool', async () => {
   backend.registrar({
     tools: [
       { id: 1, title: 'Ferramenta 1', tags: ['a'] },
@@ -35,30 +35,30 @@ Given('que existe mais que uma ferramenta', async () => {
   })
 })
 
-Then('eu verifico que mais que uma ferramenta são apresentadas', async () => {
+Then('i verify that more than one tool is presented', async () => {
   expect((await frontend.ferramentas()).length).to.be.equals(2)
 })
 
-When('eu solicito a adição de uma nova ferramenta', async () => {
+When('i request to add a new tool', async () => {
   await frontend.entrar()
   await frontend.adicionar()
 })
 
-Then('eu verifico exatamente três ferramentas são apresentadas', async () => {
+Then('i verify that exactly three tools are presented', async () => {
   expect((await frontend.ferramentas()).length).to.be.equals(3)
 })
 
-When('eu solicito a remoção de uma nova ferramenta', async () => {
+When('i request to remove a tool', async () => {
   await frontend.entrar()
   await frontend.remover()
 })
 
-When('eu procuro por uma ferramenta específica', async () => {
+When('i search by global criteria', async () => {
   await frontend.entrar()
   await frontend.procurar('Ferramenta 2')
 })
 
-When('eu procuro por uma tag específica', async () => {
+When('i search by specific tag', async () => {
   await frontend.entrar()
   await frontend.procurar('b', true)
 })
