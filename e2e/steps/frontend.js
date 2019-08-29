091e38fe7b2d3e3ea90c4ece0f91f1f38d4dd48d
mestihudson <mestihudson@gmail.com>
2019-08-29 00:27:09 -0300

    Returns to selenium version and others
      - Sets selenium version to 2.53.1 because 3.141.59-titanium has a bug about
        `setValue` function
      - Decreases default delay for frontend functions to 500 secs
      - Uses debug selenium node versions for vnc use
      - Exposes ports 5901 and 5902 for firefox and chrome nodes, respectively

--- e2e/steps/frontend.js
+++ e2e/steps/frontend.js
@@ -6,12 +6,12 @@ export default class Frontend {
     this.browser = client
   }
 
-  async entrar (delay = 10000) {
+  async entrar (delay = 500) {
     await this.browser.url(this.url)
       .pause(delay)
   }
 
-  async titulo (delay = 5000) {
+  async titulo (delay = 500) {
     let result
     await this.browser
       .pause(delay)
@@ -36,7 +36,16 @@ export default class Frontend {
 
   async procurar (criterio, tag) {
     await this.browser
-      .setValue(`//*[@data-input='criterio']`, criterio)
+      .click(`//*[@data-input='criterio']`)
+      .keys(criterio)
+      .keys(this.browser.Keys.TAB)
+      // .setValue(`//*[@data-input='criterio']`, criterio)
+      // .execute(() => {
+      //   // const element = document.querySelector(`[data-input='criterio']`)
+      //   console.log(criterio)
+      //   // element.value = criterio
+      //   // element.change()
+      // })
     if (tag) {
       await this.browser
         .click(`//*[@data-input='tags']`)
